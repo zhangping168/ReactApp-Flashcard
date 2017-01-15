faf40eeae39be51da0c319d3911aa08d034931cc
zhangping168@gmail.com <zhangping168@gmail.com>
2017-01-15 02:51:35 -0500

    Add newcard action

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
-console.log('hello react');
+const store = Redux.createStore(function(state, action){
+
+    switch(action.type){
+      case 'ADD_CARD':
+        let newCard = Object.assign({}, action.data , {
+          score:1,
+          id:+new Date
+        });
+        return Object.assign({}, state,{
+          cards:state.cards ? state.cards.concat([newCard]) :[newCard]
+        });
+      default:
+        return state || {};
+    }
+});
+
+store.subscribe(()=>{
+  console.log(store.getState());
+});
+
+
+store.dispatch({
+  type:'ADD_CARD',
+  data:{
+    title:'javascript',
+    content:'React with Redux'
+  }
+});
+
+store.dispatch({
+  type:'ADD_CARD',
+  data:{
+    title:'CSS',
+    content:'style cascade'
+  }
+});
